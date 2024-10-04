c0cfd696a8d5479ddda14808e1a1022de01a1f42
ashwanisharma9919 <109890808+ashwanisharma9919@users.noreply.github.com>
2024-10-05 01:51:00 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+   <footer>
+    <p>Created By <a href="" target='_blank'>Ashwani Sharma</a></p>
+   </footer>
+  )
+}
+
+export default Footer
